344f7cbe256bd2e9a36f1813a25f8d756fc15853
MrHatchyPantz <pwner5889@gmail.com>
2015-07-24 15:52:43 -0500

    Minor edits
    Edited the autoload.php so it initializes the database when the page
    loads, added in HMVC, cleaned up some of the folders, and changed the
    welcome-message.php to home.php

---
+++ application/modules/views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Welcome to FloofyDroogy!!</title>
+</head>
+
+<body>
+  <p>This is an awesome test!!</p>
+</body>
+</html>
---
+++ application/modules/views/templates/header.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to FloofyDroogy!!</title>
+
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/infinity/0.2.2/infinity.min.js"></script>
+
+</head>
+<body>
+
+<div class="container">
